80c4af5284715bcf4c04a53afa9c743a4d103766
Izalhadji31 <ryzalhadji@gmail.com>
2025-01-28 18:10:08 +0700

    menambahkan HomePemasokView

---
+++ app/src/main/java/com/example/uaspam/ui/View/pemasok/HomeView.kt
@@ -0,0 +1,282 @@
+package com.example.uaspam.ui.View.pemasok
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.uaspam.R
+import com.example.uaspam.model.Pemasok
+import com.example.uaspam.ui.Customwidget.CustomTopAppBar
+import com.example.uaspam.ui.Navigation.DestinasiHomePemasok
+import com.example.uaspam.ui.ViewModel.PenyediaViewModel
+import com.example.uaspam.ui.ViewModel.pemasok.HomePemasokViewModel
+import com.example.uaspam.ui.ViewModel.pemasok.HomeUiStatePemasok
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreenPemasok(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (String) -> Unit = {},
+    navigateBack: () -> Unit,
+    viewModel: HomePemasokViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CustomTopAppBar(
+                title = DestinasiHomePemasok.titleRes,
+                navigateUp = navigateBack,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                onRefresh = {
+                    viewModel.getpmsk()
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Tambah Pemasok"
+                )
+            }
+        }
+    ) { innerPadding ->
+        Column(modifier = Modifier.padding(innerPadding)) {
+
+            HomeStatusPemasok(
+                homeUiStatePemasok = viewModel.pmskUiState,
+                retryAction = { viewModel.getpmsk() },
+                modifier = Modifier.fillMaxSize(),
+                onDetailClick = onDetailClick,
+                onDeleteClick = {
+                    viewModel.deletepmsk(it.id_pemasok.toString())
+                    viewModel.getpmsk()
+                }
+            )
+        }
+    }
+}
+
+
+
+@Composable
+fun HomeStatusPemasok(
+    homeUiStatePemasok: HomeUiStatePemasok,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Pemasok) -> Unit = {},
+    onDetailClick: (String) -> Unit = {}
+){
+    when(homeUiStatePemasok) {
+        is HomeUiStatePemasok.Loading -> OnloadingPemasok(modifier = Modifier.fillMaxSize())
+
+        is HomeUiStatePemasok.Success ->
+            if (homeUiStatePemasok.pemasok.isEmpty()) {
+                return Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Text(text = "Tidak ada data Pemasok")
+                }
+            }else{
+                pmskLayout(
+                    pemasok = homeUiStatePemasok.pemasok,
+                    modifier = modifier.fillMaxWidth(),
+                    onDetailClick = {
+                        onDetailClick(it.id_pemasok.toString())
+                    },
+                    onDeleteClick = {
+                        onDeleteClick(it)
+                    }
+
+                )
+            }
+        is HomeUiStatePemasok.Error -> onErrorPemasok(retryAction, modifier = Modifier.fillMaxSize())
+    }
+}
+
+
+
+
+@Composable
+fun OnloadingPemasok(modifier: Modifier = Modifier){
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.loadd),
+        contentDescription = stringResource(R.string.loading)
+    )
+}
+
+@Composable
+fun onErrorPemasok(
+    retryAction: () -> Unit,modifier: Modifier = Modifier
+){
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.images), contentDescription = ""
+        )
+        Text(text = stringResource(R.string.loading_failed), modifier = Modifier.padding(16.dp))
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
+        }
+    }
+}
+
+
+@Composable
+fun pmskLayout(
+    pemasok: List<Pemasok>,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Pemasok) -> Unit,
+    onDeleteClick: (Pemasok) -> Unit = {},
+){
+    LazyColumn (
+        modifier = modifier,
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ){
+        items(pemasok){pemasok ->
+            pmskCard(
+                pemasok = pemasok,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { onDetailClick(pemasok) },
+                onDeleteClick = {
+                    onDeleteClick(pemasok)
+                }
+            )
+        }
+    }
+}
+
+
+@Composable
+fun pmskCard(
+    pemasok: Pemasok,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Pemasok) -> Unit = {}
+) {
+    var showDialog by remember { mutableStateOf(false) }
+
+    if (showDialog) {
+        DeleteConfirmationDialogpmsk(
+            onDeleteConfirm = {
+                showDialog = false
+                onDeleteClick(pemasok)
+            },
+            onDeleteCancel = {
+                showDialog = false
+            }
+        )
+    }
+    Card(
+        modifier = modifier,
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Text(
+                    text = pemasok.nama_produk,
+                    style = MaterialTheme.typography.titleLarge)
+                Spacer(Modifier.weight(1f))
+                IconButton(onClick = { onDeleteClick(pemasok) }) {
+                    Icon(
+                        imageVector = Icons.Default.Delete,
+                        contentDescription = null
+                    )
+                }
+                Text(
+                    text = pemasok.nama_pemasok,
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
+            Text(
+                text = pemasok.alamat_pemasok?.toString() ?: "N/A",
+                style = MaterialTheme.typography.titleMedium
+            )
+            Text(
+                text = pemasok. telepon_pemasok?.toString() ?: "N/A",
+                style = MaterialTheme.typography.titleMedium
+            )
+
+
+
+        }
+
+    }
+}
+
+@Composable
+private fun DeleteConfirmationDialogpmsk(
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    AlertDialog(onDismissRequest = { /*Do nothing*/ },
+        title = { Text("Delete Data") },
+        text = { Text("Apakah anda yakin ingin menghapus data?") },
+        dismissButton = {
+            TextButton(onClick = { onDeleteCancel() }) {
+                Text(text = "Cancel")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = { onDeleteConfirm() }) {
+                Text(text = "Yes")
+            }
+        }
+    )
+}
