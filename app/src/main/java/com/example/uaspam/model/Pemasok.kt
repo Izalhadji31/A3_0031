6a7026b4287334eb78a813560f3ae379218ed7bb
Izalhadji31 <ryzalhadji@gmail.com>
2025-01-28 17:43:26 +0700

    menambahkan data class pemasok

---
+++ app/src/main/java/com/example/uaspam/model/Pemasok.kt
@@ -0,0 +1,26 @@
+package com.example.uaspam.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Pemasok (
+    val id_pemasok: Int,
+    val nama_produk: String,
+    val nama_pemasok: String,
+    val alamat_pemasok: String,
+    val telepon_pemasok: String,
+)
+
+@Serializable
+data class AllPemasokResponse(
+    val status: Boolean,
+    val message: String,
+    val data: List<Pemasok>
+)
+
+@Serializable
+data class PemasokDetailResponse(
+    val status: Boolean,
+    val message: String,
+    val data: Pemasok
+)
