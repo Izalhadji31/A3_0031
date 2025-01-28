836f7f06244c3c808254fe705c42a1c4c6eaedb8
Izalhadji31 <ryzalhadji@gmail.com>
2025-01-28 17:43:04 +0700

    menambahkan data class kategori

---
+++ app/src/main/java/com/example/uaspam/model/Kategori.kt
@@ -0,0 +1,24 @@
+package com.example.uaspam.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Kategori (
+    val id_kategori: Int,
+    val nama_kategori: String,
+    val deskripsi_kategori: String,
+)
+
+@Serializable
+data class  AllKategoriResponse(
+    val status: Boolean,
+    val message: String,
+    val data: List<Kategori>
+)
+
+@Serializable
+data class KategoriDetailResponse(
+    val status: Boolean,
+    val message: String,
+    val data: Kategori
+)
