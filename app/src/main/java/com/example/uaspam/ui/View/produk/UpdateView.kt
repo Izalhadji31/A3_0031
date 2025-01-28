6b4d51449507cf97608f49678b5f625e7353c6ee
Izalhadji31 <ryzalhadji@gmail.com>
2025-01-28 18:07:03 +0700

    Membuat UpdateProdukView

---
+++ app/src/main/java/com/example/uaspam/ui/View/produk/UpdateView.kt
@@ -0,0 +1,61 @@
+package com.example.uaspam.ui.View.produk
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.uaspam.ui.Customwidget.CustomTopAppBar
+import com.example.uaspam.ui.Navigation.DestinasiUpdate
+import com.example.uaspam.ui.ViewModel.PenyediaViewModel
+import com.example.uaspam.ui.ViewModel.produk.UpdateProdukViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UpdateScreen(
+    onBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    onNavigate:()-> Unit,
+    viewModel: UpdateProdukViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CustomTopAppBar(
+                title = DestinasiUpdate.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = onBack,
+            )
+        }
+    ){padding ->
+        EntryBody(
+            modifier = Modifier.padding(padding),
+            insertUiState = viewModel.updateUiState,
+            onSiswaValueChange = viewModel::updateInsertprdkState,
+            pmskList = viewModel.pmskList,
+            ktgrList = viewModel.ktgrList,
+            mrkList = viewModel.mrkList,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updateprdk()
+                    delay(600)
+                    withContext(Dispatchers.Main){
+                        onNavigate()
+                    }
+                }
+            }
+        )
+    }
+}
