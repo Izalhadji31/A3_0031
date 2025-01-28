368d8b50d61be34eca883b96549600505b0645e5
Izalhadji31 <ryzalhadji@gmail.com>
2025-01-28 18:07:24 +0700

    Membuat HomeProdukView

---
+++ app/src/main/java/com/example/uaspam/ui/View/produk/HomeView.kt
@@ -0,0 +1,321 @@
+package com.example.uaspam.ui.View.produk
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.uaspam.R
+import com.example.uaspam.model.Produk
+import com.example.uaspam.ui.Customwidget.CustomTopAppBar
+import com.example.uaspam.ui.Navigation.DestinasiHome
+import com.example.uaspam.ui.ViewModel.PenyediaViewModel
+import com.example.uaspam.ui.ViewModel.produk.HomeProdukViewModel
+import com.example.uaspam.ui.ViewModel.produk.HomeUiState
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    onNavigateToPemasok: () -> Unit,  // Tambahkan fungsi untuk navigasi ke pemasok
+    onNavigateToKategori: () -> Unit, // Tambahkan fungsi untuk navigasi ke kategori
+    onNavigateToMerk: () -> Unit, // Tambahkan fungsi untuk navigasi ke merk
+    modifier: Modifier = Modifier,
+    onDetailClick: (String) -> Unit = {},
+    viewModel: HomeProdukViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    Scaffold(
+        topBar = {
+            CustomTopAppBar(
+                title = DestinasiHome.titleRes,
+                canNavigateBack = false,
+                onRefresh = { viewModel.getprdk() }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Add Produk")
+            }
+        },
+        modifier = modifier.fillMaxSize()
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            // Tambahkan Button untuk navigasi ke Pemasok
+            Button(
+                onClick = onNavigateToPemasok,
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp)
+            ) {
+                Text("Go to Pemasok Page")
+            }
+            // Tambahkan Button untuk navigasi ke Kategori
+            Button(
+                onClick = onNavigateToKategori,
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp)
+            ) {
+                Text("Go to Kategori Page")
+            }
+            Button(
+                onClick = onNavigateToMerk,
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp)
+            ) {
+                Text("Go to Merk Page")
+            }
+
+            // Konten HomeStatus
+            HomeStatus(
+                homeUiState = viewModel.prdkUiState,
+                retryAction = { viewModel.getprdk() },
+                modifier = Modifier.fillMaxSize(),
+                onDetailClick = onDetailClick,
+                onDeleteClick = {
+                    viewModel.deleteprdk(it.id_produk.toString())
+                    viewModel.getprdk()
+                }
+            )
+        }
+    }
+}
+
+
+@Composable
+fun HomeStatus(
+    homeUiState: HomeUiState,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Produk) -> Unit = {},
+    onDetailClick: (String) -> Unit
+){
+    when (homeUiState) {
+        is HomeUiState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
+        is HomeUiState.Success ->
+            if (homeUiState.produk.isEmpty()){
+                return Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Text(text = "Tidak ada data Produk" )
+                }
+            } else {
+                PrdkLayout(
+                    produk = homeUiState.produk, modifier = modifier.fillMaxWidth(),
+                    onDetailClick = {
+                        onDetailClick(it.id_produk.toString())
+                    },
+                    onDeleteClick = {
+                        onDeleteClick(it)
+                    }
+                )
+            }
+        is HomeUiState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
+    }
+}
+
+@Composable
+fun OnLoading(
+    modifier: Modifier = Modifier
+) {
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.loadd),
+        contentDescription = stringResource(R.string.loading)
+    )
+}
+
+@Composable
+fun OnError(
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column (
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.images), contentDescription = ""
+        )
+        Text(
+            text = stringResource(R.string.loading_failed), modifier = Modifier.padding(16.dp)
+        )
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
+        }
+    }
+}
+
+@Composable
+fun PrdkLayout(
+    produk: List<Produk>,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Produk) -> Unit,
+    onDeleteClick: (Produk) -> Unit = {}
+) {
+    LazyColumn (
+        modifier = modifier.fillMaxSize(),
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(produk) { produk ->
+            PrdkCard(
+                produk = produk,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { onDetailClick(produk) },
+                onDeleteClick = { onDeleteClick(produk) }
+            )
+        }
+    }
+}
+
+@Composable
+fun PrdkCard(
+    produk: Produk,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Produk) -> Unit = {}
+) {
+    var showDialog by remember { mutableStateOf(false) }
+
+    if (showDialog) {
+        DeleteConfirmationDialog(
+            onDeleteConfirm = {
+                showDialog = false
+                onDeleteClick(produk)
+            },
+            onDeleteCancel = {
+                showDialog = false
+            }
+        )
+    }
+
+    Card(
+        modifier = modifier,
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surfaceVariant)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = produk.nama_produk,
+                    style = MaterialTheme.typography.titleLarge
+                )
+                Spacer(Modifier.weight(1f))
+                IconButton(onClick = { showDialog = true }) {
+                    Icon(
+                        imageVector = Icons.Default.Delete,
+                        contentDescription = "HAPUS PRODUK"
+                    )
+                }
+            }
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = produk.deskripsi_produk,
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.weight(1f)
+                )
+                Icon(
+                    imageVector = Icons.Default.ShoppingCart, // Ikon keranjang bawaan
+                    contentDescription = "KERANJANG",
+                    modifier = Modifier.size(24.dp)
+                )
+
+            }
+
+            Text(
+                text = "HARGA: Rp${produk.harga}",
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Text(
+                text = "STOK: ${produk.stok}",
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Text(
+                text = "PEMASOK: ${produk.id_pemasok}",
+                style = MaterialTheme.typography.bodySmall
+            )
+            Text(
+                text = "MEREK: ${produk.id_merk}",
+                style = MaterialTheme.typography.bodySmall
+            )
+        }
+    }
+}
+
+
+
+@Composable
+fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    AlertDialog(onDismissRequest = { /*Do nothing*/ },
+        title = { Text("Delete Data")},
+        text = { Text("Apakah anda yakin ingin menghapus data?")},
+        dismissButton = {
+            TextButton(onClick = { onDeleteCancel() }) {
+                Text(text = "Cancel")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = { onDeleteConfirm() }) {
+                Text(text = "Yes")
+            }
+        }
+    )
+}
