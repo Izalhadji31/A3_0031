dcf71b67c45466d002e7750442570179829545e3
Izalhadji31 <ryzalhadji@gmail.com>
2025-01-28 18:07:14 +0700

    Membuat DetailProdukView

---
+++ app/src/main/java/com/example/uaspam/ui/View/produk/DetailView.kt
@@ -0,0 +1,165 @@
+package com.example.uaspam.ui.View.produk
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.uaspam.model.Produk
+import com.example.uaspam.ui.Customwidget.CustomTopAppBar
+import com.example.uaspam.ui.Navigation.DestinasiDetail
+import com.example.uaspam.ui.ViewModel.PenyediaViewModel
+import com.example.uaspam.ui.ViewModel.produk.DetailProdukViewModel
+import com.example.uaspam.ui.ViewModel.produk.DetailUiState
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailScreen(
+    navigateBack: () -> Unit,
+    onEditClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailProdukViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    Scaffold(
+        topBar = {
+            CustomTopAppBar(
+                title = DestinasiDetail.titleRes,
+                canNavigateBack = true,
+                navigateUp = navigateBack,
+                onRefresh = {
+                    viewModel.getprodukbyid_produk()
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onEditClick,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit Kontak"
+                )
+            }
+        }
+    ) { innerPadding ->
+        DetailStatus(
+            modifier = Modifier.padding(innerPadding),
+            detailUiState = viewModel.produkDetailState,
+            retryAction = { viewModel.getprodukbyid_produk() }
+        )
+    }
+}
+
+@Composable
+fun DetailStatus(
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    detailUiState: DetailUiState
+) {
+    when (detailUiState) {
+        is DetailUiState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
+
+        is DetailUiState.Success -> {
+            if (detailUiState.produk.data.id_produk == 0) {
+                Box(
+                    modifier = modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text("Data tidak ditemukan.")
+                }
+            } else {
+                ItemDetailprdk(
+                    produk = detailUiState.produk.data,
+                    modifier = modifier.fillMaxWidth()
+                )
+            }
+        }
+
+        is DetailUiState.Error -> OnError(retryAction , modifier = modifier.fillMaxSize())
+    }
+}
+
+@Composable
+fun ItemDetailprdk(
+    modifier: Modifier = Modifier,
+    produk: Produk) {
+    Card(
+        modifier = modifier.padding(16.dp),
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            ComponentDetailMhs(
+                judul = "ID PRODUK",
+                isinya = produk.id_produk.toString()
+            )
+            ComponentDetailMhs(
+                judul = "NAMA PRODUK", isinya = produk.nama_produk
+            )
+            ComponentDetailMhs(
+                judul = "DESKRIPSI", isinya = produk.deskripsi_produk
+            )
+            ComponentDetailMhs(
+                judul = "HARGA", isinya = produk.harga.toString()
+            )
+            ComponentDetailMhs(
+                judul = "STOK", isinya = produk.stok.toString()
+            )
+            ComponentDetailMhs(
+                judul = "ID KATEGORI", isinya = produk.id_kategori.toString()
+            )
+            ComponentDetailMhs(
+                judul = "ID PEMASOK", isinya = produk.id_pemasok.toString()
+            )
+            ComponentDetailMhs(
+                judul = "ID MEREK", isinya = produk.id_merk.toString()
+            )
+
+        }
+    }
+}
+
+@Composable
+fun ComponentDetailMhs(
+    modifier: Modifier =Modifier,
+    judul: String,
+    isinya: String,
+){
+    Column(modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start)
+    {
+        Text(
+            text = "$judul : ",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+        Text(
+            text = isinya,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
