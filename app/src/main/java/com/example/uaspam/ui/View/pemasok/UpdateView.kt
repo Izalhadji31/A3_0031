3fd70111e5618cdfe3cb24d84395644a76027bd2
Izalhadji31 <ryzalhadji@gmail.com>
2025-01-28 18:09:48 +0700

    menambahkan UpdatePemasokView

---
+++ app/src/main/java/com/example/uaspam/ui/View/pemasok/UpdateView.kt
@@ -0,0 +1,59 @@
+package com.example.uaspam.ui.View.pemasok
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.uaspam.ui.Customwidget.CustomTopAppBar
+import com.example.uaspam.ui.Navigation.DestinasiUpdatePemasok
+import com.example.uaspam.ui.ViewModel.PenyediaViewModel
+import com.example.uaspam.ui.ViewModel.pemasok.UpdatePemasokViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UpdateScreenPemasok(
+    onBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    onNavigate:()-> Unit,
+    viewModel: UpdatePemasokViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CustomTopAppBar(
+                title = DestinasiUpdatePemasok.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = onBack,
+            )
+        }
+    ){padding ->
+        EntryBodyPemasok(
+            modifier = Modifier.padding(padding),
+            InsertUiStatePemasok = viewModel.updateUiStatepemasok,
+            onValueChange = viewModel::updateInsertpmskState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updatepmsk()
+                    delay(600)
+                    withContext(Dispatchers.Main){
+                        onNavigate()
+                    }
+                }
+            }
+        )
+    }
+}
