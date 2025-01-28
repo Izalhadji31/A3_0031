9647b1fe45e19933f2fdd1bdfb2c4aa98dd5ff96
Izalhadji31 <ryzalhadji@gmail.com>
2025-01-28 17:42:44 +0700

    menambahkan data class produk

---
+++ app/src/main/java/com/example/uaspam/model/Produk.kt
@@ -0,0 +1,29 @@
+package com.example.uaspam.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Produk (
+    val id_produk: Int,
+    val nama_produk: String,
+    val deskripsi_produk: String,
+    val harga: Int,
+    val stok: Int,
+    val id_kategori: Int,
+    val id_pemasok: Int,
+    val id_merk: Int,
+)
+
+@Serializable
+data class AllProdukResponse(
+    val status: Boolean,
+    val message: String,
+    val data: List<Produk>
+)
+
+@Serializable
+data class ProdukDetailResponse(
+    val status: Boolean,
+    val message: String,
+    val data: Produk
+)
