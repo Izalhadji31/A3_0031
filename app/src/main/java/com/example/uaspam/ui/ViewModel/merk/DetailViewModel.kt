4a7f183c51ad88c0298ddd83e9d44d3f168d90ac
Izalhadji31 <ryzalhadji@gmail.com>
2025-01-28 18:00:36 +0700

    menambahkan DetailMerkViewModel

---
+++ app/src/main/java/com/example/uaspam/ui/ViewModel/merk/DetailViewModel.kt
@@ -0,0 +1,49 @@
+package com.example.uaspam.ui.ViewModel.merk
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.uaspam.model.MerkDetailResponse
+import com.example.uaspam.repository.MerkRepository
+import com.example.uaspam.ui.Navigation.DestinasiDetailMerk
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
+
+sealed class DetailUiStateMerk {
+    data class Success(val merk: MerkDetailResponse) : DetailUiStateMerk()
+    object Error : DetailUiStateMerk()
+    object Loading : DetailUiStateMerk()
+}
+
+class DetailMerkViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val ktgr: MerkRepository
+) : ViewModel() {
+
+    var merkDetailState: DetailUiStateMerk by mutableStateOf(DetailUiStateMerk.Loading)
+        private set
+
+    private val _id_merk: String = checkNotNull(savedStateHandle[DestinasiDetailMerk.ID_merk])
+
+    init {
+        getmerkbyid_merk()
+    }
+
+    fun getmerkbyid_merk() {
+        viewModelScope.launch {
+            merkDetailState = DetailUiStateMerk.Loading
+            merkDetailState = try {
+                val merk = ktgr.getmerkByid_merk(_id_merk)
+                DetailUiStateMerk.Success(merk )
+            } catch (e: IOException) {
+                DetailUiStateMerk.Error
+            } catch (e: HttpException) {
+                DetailUiStateMerk.Error
+            }
+        }
+    }
+}
